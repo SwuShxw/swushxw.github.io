8c3039576f0096288b64d30c4aea03b5dc10281f
swuixw <swu_shxw@foxmail.com>
2019-01-25 01:06:50 +0800

    Site updated: 2019-01-25 01:06:48

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,3 +1,17 @@
-// build time:Fri Jan 25 2019 01:02:36 GMT+0800 (GMT+08:00)
-var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/trhx2.png");document.title="ヽ(●-`Д´-)ノ你丑你就走！";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/img/trhx2.png");document.title="ヾ(Ő∀Ő3)ノ你帅就回来！"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
-//rebuild by neat 
+<!--浏览器搞笑标题-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
--- js/xoxo.js
+++
@@ -1,3 +0,0 @@
-// build time:Fri Jan 25 2019 01:02:36 GMT+0800 (GMT+08:00)
-
-//rebuild by neat 
